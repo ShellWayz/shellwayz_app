794682903269ab376889764c0ee99ded328bd75f
Arshia Eihami <arshiaihammi@gmail.com>
2026-05-18 23:05:59 +0300

    feat(master-key): add dependency injection setup

--- lib/core/di/injector.dart
+++ lib/core/di/injector.dart
@@ -1,5 +1,31 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
+
+import '../security/master_key_service.dart';
 
 final getIt = GetIt.instance;
 
-Future<void> setupDI() async {}
+Future<void> setupDI() async {
+  if (getIt.isRegistered<FlutterSecureStorage>()) return;
+
+  getIt.registerLazySingleton<FlutterSecureStorage>(
+    () => const FlutterSecureStorage(
+      aOptions: AndroidOptions(
+        resetOnError: true,
+        migrateOnAlgorithmChange: true,
+      ),
+      iOptions: IOSOptions(
+        accessibility: KeychainAccessibility.first_unlock_this_device,
+        synchronizable: false,
+      ),
+      mOptions: MacOsOptions(
+        accessibility: KeychainAccessibility.first_unlock_this_device,
+        synchronizable: false,
+      ),
+    ),
+  );
+
+  getIt.registerLazySingleton<MasterKeyService>(
+    () => MasterKeyService(getIt<FlutterSecureStorage>()),
+  );
+}
